ccf4f045a2b4546f1905340c80e41c868ceea94b
BaironCano <baironvin13616@gmail.com>
2023-03-23 18:10:41 -0500

    feat: get the information out of the works (date, title, salary range, workplace and the employer)

--- popup/index.js
+++ popup/index.js
@@ -4,10 +4,23 @@ btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: alertHelloWorld,
+    func: filterJobs,
   });
 });
 
-function alertHelloWorld() {
-  alert("Hello World");
+function filterJobs() {
+  const arrayJobs = [...document.querySelectorAll("[id^='jobcard-']")];
+  const organizedJobs = arrayJobs.map((job) => {
+    return {
+      date: job.querySelector("label[class*='text-0-2-82']")?.innerText,
+      title: job.querySelector("h2[class*='text-0-2-82']")?.innerText,
+      salaryRange: job.querySelector("span[class*='text-0-2-82']")?.innerText,
+      workplace: job.querySelector("p[class*='text-0-2-82']")?.innerText,
+      employer:
+        job.querySelector("a[class*='locContainer-0-2-565']")?.innerText ??
+        "Empresa confidencial",
+    };
+  });
+
+  console.log(organizedJobs);
 }

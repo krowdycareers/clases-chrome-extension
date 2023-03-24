a056574aa51eaf6c5d0ee10b4716659e1dfd44be
Diego Berastain <berastaindiego@gmail.com>
2023-03-24 18:46:52 -0500

    scraping info from website

--- popup/index.js
+++ popup/index.js
@@ -8,6 +8,56 @@ btnScripting.addEventListener("click", async () => {
   });
 });
 
+// function alertHelloWorld() {
+//   const elemCardJobs = [...document.querySelectorAll("[id*='jobcard-']")];
+//   const jobs = elemCardJobs.map((job) => {
+//     console.log(job)
+//     const [
+//       { href: url },
+//       {
+//         children: [
+//           {
+//             children: [{ innerText: fecha }, { innerText: title }],
+//           },
+//         ],
+//       },
+//     ] = job.children;
+
+//     return { url, fecha, title };
+//   });
+
+//   console.log(jobs);
+// }
+
 function alertHelloWorld() {
-  alert("Hello World");
+  const elemCardJobs = [...document.querySelectorAll("[id*='jobcard-']")];
+
+  const jobs = elemCardJobs.map((job) => {
+    const currentDate = job.querySelector("label[class*='date-']").innerText;
+    const currentTitle = job.querySelector("h2").innerText;
+    const currentSalary = job.querySelector("span[class*='salary-']").innerText;
+    const currentcompanyName = job.querySelector("div[style='flex:1'] label").innerText;
+    const currentCityAndState = job.querySelector("div[style='flex:1'] p").innerText;
+    const currentBenefits = [...job.querySelectorAll("li[class*='li-']")];
+
+
+    const benefitsText =
+      (currentBenefits.length > 0)
+        ? currentBenefits.reduce((acc, benefit) => {
+            acc.push(benefit.innerText);
+            return acc;
+          }, [])
+        : "Sin Beneficios";
+
+    return {
+      date: currentDate,
+      title: currentTitle,
+      salary: currentSalary,
+      benefits: benefitsText,
+      location: currentCityAndState,
+      company: currentcompanyName,
+    };
+  });
+
+  console.log(jobs);
 }

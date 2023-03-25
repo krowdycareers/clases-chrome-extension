014abbe27abc4d5d5ca79b3d0f37f81f78e6a871
Diego Berastain <berastaindiego@gmail.com>
2023-03-24 20:28:21 -0500

    update again

--- popup/index.js
+++ popup/index.js
@@ -10,74 +10,6 @@ btnScripting.addEventListener("click", async () => {
   });
 });
 
-<<<<<<< HEAD
-function alertHelloWorld() {
-  const elemCardJobs = [...document.querySelectorAll("[id*='jobcard-']")];
-  const jobs = elemCardJobs.map((job) => {
-    console.log(job);
-    const [
-      { href: url },
-      {
-        children: [
-          {
-            children: [
-              { innerText: fecha },
-              { innerText: title },
-              { innerText: salario },
-              { innerText: beneficios },
-              {},
-              {
-                children: [elementEmpresaCiudad]
-              }
-            ],
-          },
-        ],
-      },
-    ] = job.children;
-
-    const empresa = elementEmpresaCiudad?.querySelector("label")?.innerText;
-    const ciudad = elementEmpresaCiudad?.querySelector("p")?.innerText;
-
-    return { url, fecha, title, salario, beneficios, empresa, ciudad };
-  });
-
-  console.log(jobs);
-}
-
-
-// mi versión
-// function alertHelloWorld() {
-//   const elemCardJobs = [...document.querySelectorAll("[id*='jobcard-']")];
-
-//   const jobs = elemCardJobs.map((job) => {
-//     const currentDate = job.querySelector("label[class*='date-']").innerText;
-//     const currentTitle = job.querySelector("h2").innerText;
-//     const currentSalary = job.querySelector("span[class*='salary-']").innerText;
-//     const currentcompanyName = job.querySelector("div[style='flex:1'] label").innerText;
-//     const currentCityAndState = job.querySelector("div[style='flex:1'] p").innerText;
-//     const currentBenefits = [...job.querySelectorAll("li[class*='li-']")];
-
-//     const benefitsText =
-//       (currentBenefits.length > 0)
-//         ? currentBenefits.reduce((acc, benefit) => {
-//             acc.push(benefit.innerText);
-//             return acc;
-//           }, [])
-//         : "Sin Beneficios";
-
-//     return {
-//       date: currentDate,
-//       title: currentTitle,
-//       salary: currentSalary,
-//       benefits: benefitsText,
-//       location: currentCityAndState,
-//       company: currentcompanyName,
-//     };
-//   });
-
-//   console.log(jobs);
-// }
-=======
 btnScriptingBackground.addEventListener("click", async () => {
   var port = chrome.runtime.connect({ name: "popup-background" });
   port.postMessage({ message: "Hola BD" });
@@ -85,4 +17,3 @@ btnScriptingBackground.addEventListener("click", async () => {
     alert(message);
   });
 });
->>>>>>> ad5ec94cd7b5ad0fb783e44dcc77388f80689647

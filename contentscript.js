95afe89b8660ae37bad987f3ef879571bd3aa283
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2023-03-24 21:29:45 -0500

    [feat] comunication

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,51 @@
-console.log("Ejecutando el content script 1.0");
+console.log("Ejecutando el content script 2.0");
+function getJobInformation() {
+  const elemCardJobs = [...document.querySelectorAll('[id*="jobcard-"]')];
+  const jobs = elemCardJobs.map((cardJob) => {
+    const [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              { innerText: fecha },
+              { innerText: title },
+              { innerText: salario },
+              { innerText: beneficios },
+              {},
+              {
+                children: [elementEmpresCiudad],
+              },
+            ],
+          },
+        ],
+      },
+    ] = cardJob.children;
+
+    const empresa = elementEmpresCiudad?.querySelector("label")?.innerText;
+    const ciudad = elementEmpresCiudad?.querySelector("p")?.innerText;
+    return { url, fecha, title, salario, beneficios, empresa, ciudad };
+  });
+
+  return jobs;
+}
+
+//Connect to background
+const portBackground = chrome.runtime.connect({ name: "content-background" });
+
+portBackground.onMessage.addListener(async ({ message }) => {
+  if ((message = "nextpage")) {
+    const nextPageButton = document.querySelector("[class*=next-]");
+    nextPageButton.click();
+  }
+});
 
 chrome.runtime.onConnect.addListener(function (port) {
   port.onMessage.addListener(function ({ message }) {
-    alert(`${port.name}: message`);
-    if (message === "hola") port.postMessage({ message: "Como estas?" });
+    if (message === "getJobs") {
+      const jobs = getJobInformation();
+      port.postMessage({ message: "ok", data: jobs });
+      portBackground.postMessage({ message: "finish" });
+    }
   });
-});
-
-//Connect to background
-const port = chrome.runtime.connect({ name: "content-background" });
-port.postMessage({ message: "Hola Background" });
-port.onMessage.addListener(async ({ message }) => {
-  alert(message);
 });

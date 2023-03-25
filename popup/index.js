a4e56340222d4e80662102c9ed613424a74f29e7
Luiggi Yantas Inocente <luiggiyantas@gmail.com>
2023-03-24 22:47:43 -0500

    [feat] getJobs

--- popup/index.js
+++ popup/index.js
@@ -4,10 +4,40 @@ btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: alertHelloWorld,
+    func: getJobs,
   });
 });
 
-function alertHelloWorld() {
-  alert("Hello World");
+function getJobs() {
+  const jobCards = [...document.querySelectorAll('[id*="jobcard-"]')];
+
+  const jobsInfo = jobCards.map((jobCard) => {
+    const [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              { innerText: date },
+              { innerText: title },
+              { innerText: salaryRange },
+              { innerText: description = null } = {}, // tal vez no esta presente
+              {},
+              {
+                children: [companyAndLocation],
+              },
+            ],
+          },
+        ],
+      },
+    ] = jobCard.children;
+
+    const company = companyAndLocation?.querySelector("label")?.innerText;
+    const location = companyAndLocation?.querySelector("p")?.innerText;
+
+    return { url, date, title, salaryRange, description, company, location };
+  });
+
+  console.log(jobsInfo);
+  return jobsInfo;
 }

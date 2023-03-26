acdf34a55a06bb41e8e63a1896b5243613b2f346
Nelson Alarcon Cahuana <nels@asus.localhost>
2023-03-25 23:08:54 -0500

    feat(popup): get summary jobs

--- popup/index.js
+++ popup/index.js
@@ -4,10 +4,41 @@ btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: alertHelloWorld,
+    func: getSummaryJobs,
   });
 });
 
-function alertHelloWorld() {
-  alert("Hello World");
-}
+function getSummaryJobs() {
+  const jobs = [...document.querySelectorAll('[id*="jobcard-"]')];
+  const summaryJob = jobs.map(({ children }) => {
+    const [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              {
+                children: [{ children: [{ innerText: date }] }]
+              },
+              { innerText: title },
+              { innerText: salaryRange },
+              { innerText: utils },
+              { innerText: beforeCompany },
+              { innerText: afterCompany }
+            ]
+          }
+        ]
+      }
+    ] = children;
+    return {
+      url,
+      date,
+      title,
+      salaryRange: salaryRange.split('\n')[0],
+      utils: utils.length > 0 ? utils.split('\n') : 'No menciona',
+      company: utils.length > 0 ? afterCompany.split('\n\n')[0] : beforeCompany.split('\n\n')[0],
+      city: utils.length > 0 ? (!!afterCompany.split('\n\n')[1] ? afterCompany.split('\n\n')[1] : 'No menciona') : (!!beforeCompany.split('\n\n')[1] ? beforeCompany.split('\n\n')[1] : 'No menciona'),
+    }
+  })
+  console.log(summaryJob);
+};

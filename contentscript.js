c3f6679a0b65559c56749cc5bc28152c47838aa9
BaironCano <baironvin13616@gmail.com>
2023-03-28 17:01:16 -0500

    challenge: reto de scrapping y paginación mediante chrome extensions

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,39 @@
 console.log("Ejecutando el content script 1.0");
 
-chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function ({ message }) {
-    alert(`${port.name}: message`);
-    if (message === "hola") port.postMessage({ message: "Como estas?" });
+function getJobInformation() {
+  const arrayJobs = [...document.querySelectorAll("[id^='jobcard-']")];
+  const organizedJobs = arrayJobs.map((job) => {
+    return {
+      date: job.querySelector("label[class*='text-0-2-82']")?.innerText,
+      title: job.querySelector("h2[class*='text-0-2-82']")?.innerText,
+      salaryRange: job.querySelector("span[class*='text-0-2-82']")?.innerText,
+      workplace: job.querySelector("p[class*='text-0-2-82']")?.innerText,
+      employer:
+        job.querySelector("a[class*='locContainer-0-2-565']")?.innerText ??
+        "Empresa confidencial",
+    };
   });
-});
+
+  return organizedJobs;
+}
 
 //Connect to background
-const port = chrome.runtime.connect({ name: "content-background" });
-port.postMessage({ message: "Hola Background" });
-port.onMessage.addListener(async ({ message }) => {
-  alert(message);
+const portBackground = chrome.runtime.connect({ name: "content-background" });
+portBackground.postMessage({ message: "finish" });
+
+portBackground.onMessage.addListener(async ({ message }) => {
+  if (message === "next page") {
+    const nextPageButton = document.querySelector("li[class*='next-']");
+    nextPageButton.click();
+  }
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function ({ message }) {
+    if (message === "getJobs") {
+      const jobs = getJobInformation();
+      port.postMessage({ message: "ok", data: jobs });
+      portBackground.postMessage({ message: "finish" });
+    }
+  });
 });
--- popup/index.js
+++ popup/index.js
@@ -1,18 +1,19 @@
 const btnScripting = document.getElementById("btncomunicacion");
 const btnScriptingBackground = document.getElementById("btncomunicacionbckg");
+const pMessage = document.getElementById("mensaje");
 
 btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let port = chrome.tabs.connect(tab.id, { name: "popup" });
-  port.postMessage({ message: "hola" });
-  port.onMessage.addListener(function ({ message }) {
-    alert(message);
+  port.postMessage({ message: "getJobs" });
+  port.onMessage.addListener(function ({ message, data }) {
+    if (message === "ok") pMessage.innerText = JSON.stringify(data, null, 2);
   });
 });
 
 btnScriptingBackground.addEventListener("click", async () => {
   var port = chrome.runtime.connect({ name: "popup-background" });
-  port.postMessage({ message: "Hola BD" });
+  port.postMessage({ message: "Start scrap" });
   port.onMessage.addListener(function ({ message }) {
     alert(message);
   });

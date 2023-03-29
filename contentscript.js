65341f1c10dc1f5fa0526ebb43900e3907483a59
pitt <roypalominor28@gmail.com>
2023-03-28 19:02:06 -0500

    Reto del viernes(24-03-2023) terminado

--- contentscript.js
+++ contentscript.js
@@ -1,33 +1,190 @@
 console.log("Ejecutando el content script 2.0");
 function getJobInformation() {
   const elemCardJobs = [...document.querySelectorAll('[id*="jobcard-"]')];
-  const jobs = elemCardJobs.map((cardJob) => {
-    const [
-      { href: url },
-      {
-        children: [
-          {
-            children: [
-              { innerText: fecha },
-              { innerText: title },
-              { innerText: salario },
-              { innerText: beneficios },
-              {},
-              {
-                children: [elementEmpresCiudad],
-              },
-            ],
-          },
-        ],
-      },
-    ] = cardJob.children;
+  const listJobs = elemCardJobs.map((cardJob) => {
+    /* Crear un array a partir de los hijos del elemento con el id "jobcard-". */
+    const auxAray = Array.from(cardJob.children[1].children[0].children);
 
-    const empresa = elementEmpresCiudad?.querySelector("label")?.innerText;
-    const ciudad = elementEmpresCiudad?.querySelector("p")?.innerText;
-    return { url, fecha, title, salario, beneficios, empresa, ciudad };
+    /* Comprobación de si el elemento tiene beneficios o no a travez de la longitud del auxArray. */
+    const hasBenefit = auxAray.length == 7 ? true : false;
+
+    /* A destructuring assignment. */
+    if (hasBenefit) {
+      const [
+        { href: url },
+        {
+          children: [
+            {
+              children: [
+                date,
+                nameJob,
+                rangeSalary,
+                benefits,
+                ,
+                {
+                  children: [
+                    {
+                      children: [
+                        {
+                          children: [
+                            {
+                              children: [
+                                {
+                                  children: [factory, location],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ] = cardJob.children;
+      return {
+        fecha: date.innerText,
+        empresa: factory.innerText,
+        puesto: nameJob.innerText,
+        rangoSalarial: rangeSalary.innerText,
+        beneficios: benefits.innerText,
+        lugar: location.innerText,
+        url,
+      };
+    } else {
+      const [
+        { href: url },
+        {
+          children: [
+            {
+              children: [
+                date,
+                nameJob,
+                rangeSalary,
+                ,
+                {
+                  children: [
+                    {
+                      children: [
+                        {
+                          children: [
+                            {
+                              children: [
+                                {
+                                  children: [factory, location],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ] = cardJob.children;
+      return {
+        fecha: date.innerText,
+        empresa: factory.innerText,
+        puesto: nameJob.innerText,
+        rangoSalarial: rangeSalary.innerText,
+        beneficios: "No se muestran",
+        lugar: location.innerText,
+        url,
+      };
+    }
   });
+  return listJobs;
+}
 
-  return jobs;
+/**
+ * It takes an array of objects, and returns an array of objects with the same properties, but with the
+ * count of how many times each object appears in the original array.
+ * @returns [
+ *   {
+ *     "pais": "Argentina",
+ *     "rangoSalarial": "Entre .000.000 y .500.000",
+ *     "cantidad": 1
+ *   },
+ *   {
+ *     "pais": "Argentina",
+ *     "rangoSalarial": "Entre 
+ */
+function filterJobCountrySalary(){
+  const dataCountryFilter = []
+  const allJobs = getJobInformation();
+  allJobs.forEach((item) => {
+    let repetir = allJobs.filter((reg) => {
+        return (
+        item.lugar.toLowerCase() === reg.lugar.toLowerCase() &&
+        item.rangoSalarial.toLowerCase() === reg.rangoSalarial.toLowerCase()
+        );
+    });
+
+    let cant = repetir.length;
+
+    if (!dataCountryFilter.some((e) => e.lugar === item.lugar && e.rangoSalarial === item.rangoSalarial)) {
+        let auxArray = {
+        pais: item.lugar,
+        rangoSalarial: item.rangoSalarial,
+        cantidad: cant,
+        };
+        dataCountryFilter.push(auxArray);
+    }
+})
+  console.log(dataCountryFilter)
+  return dataCountryFilter;
+}
+
+/**
+ * It takes the result of the previous function, and creates a new object with the country as the key,
+ * and the salary range as the value.
+ * @returns [
+ *   {
+ *     "pais": "Argentina",
+ *     "rangoSalarial": {
+ *       "0-1000": 1,
+ *       "1000-2000": 1,
+ *       "2000-3000": 1,
+ *       "3000-4000": 1,
+ *       "4000-5000": 1,
+ *       "5000-6000": 1,
+ */
+function filterJobCountry(){
+  const jobs = filterJobCountrySalary()
+  let result = [];
+  let obj = {};
+  
+  
+  jobs.forEach(function(item) {
+    if(!obj[item.pais]) {
+      obj[item.pais] = {};
+    }
+    
+    if(!obj[item.pais][item.rangoSalarial]) {
+      
+      obj[item.pais][item.rangoSalarial] = 0;
+    }
+    
+    obj[item.pais][item.rangoSalarial] += item.cantidad;
+  });
+  
+  for(let pais in obj) {
+    let tempObj = {};
+    tempObj.pais = pais;
+    tempObj.rangoSalarial = obj[pais];
+    result.push(tempObj);
+  }
+  console.log(result)
+  return result;
+
+
 }
 
 //Connect to background
@@ -43,7 +200,7 @@ portBackground.onMessage.addListener(async ({ message }) => {
 chrome.runtime.onConnect.addListener(function (port) {
   port.onMessage.addListener(function ({ message }) {
     if (message === "getJobs") {
-      const jobs = getJobInformation();
+      const jobs = filterJobCountry();
       port.postMessage({ message: "ok", data: jobs });
       portBackground.postMessage({ message: "finish" });
     }
--- popup/index.js
+++ popup/index.js
@@ -1,13 +1,44 @@
 const btnScripting = document.getElementById("btncomunicacion");
 const btnScriptingBackground = document.getElementById("btncomunicacionbckg");
-const pMensaje = document.getElementById("mensajes");
+// const pMensaje = document.getElementById("mensajes");
+const divDatos = document.getElementById("mensaje_datos")
+
 
 btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let port = chrome.tabs.connect(tab.id, { name: "popup" });
   port.postMessage({ message: "getJobs" });
   port.onMessage.addListener(({ message, data }) => {
-    if (message == "ok") pMensaje.innerText = JSON.stringify(data, null, 2);
+    if (message == "ok") {
+      const dataJob = JSON.parse(JSON.stringify(data, null, 2))
+      // pMensaje.innerText =JSON.stringify(data, null, 2)
+      // Recorrer la lista de datos filtrados
+      for (let i = 0; i < dataJob.length; i++) {
+        const pais = dataJob[i].pais;
+        const rangoSalarial = dataJob[i].rangoSalarial;
+
+        // Crear un elemento de título para el país
+        const tituloPais = document.createElement("h2");
+        tituloPais.innerText = pais;
+
+        // Agregar el título del país al div de datos
+        divDatos.appendChild(tituloPais);
+
+        // Recorrer los rangos salariales del país y crear elementos para mostrarlos
+        for (let salario in rangoSalarial) {
+          const cantidad = rangoSalarial[salario];
+
+          // Crear un elemento de texto para el rango salarial y su cantidad
+          const textoSalario = document.createElement("p");
+          textoSalario.innerText = `${salario} --------------------> ${cantidad}`;
+
+          // Agregar el elemento de texto al div de datos
+          divDatos.appendChild(textoSalario);
+        }
+      }
+    }
+      
+      
   });
 });
 
@@ -16,5 +47,8 @@ btnScriptingBackground.addEventListener("click", async () => {
   port.postMessage({ message: "Hola BD" });
   port.onMessage.addListener(function ({ message }) {
     alert(message);
+    
   });
 });
+
+

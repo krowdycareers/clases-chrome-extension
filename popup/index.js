8cbc7ed800bd8155c95fb8a5aa94824a3e61b198
Fernando <fernandoavenda2112@gmail.com>
2023-05-02 15:46:48 -0400

    aporte para la extension con su funcion de scrapt

--- popup/index.js
+++ popup/index.js
@@ -1,13 +1,16 @@
+const pMessageElement = document.getElementById("message");
 const btnScripting = document.getElementById("btnscript");
 
 btnScripting.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: alertHelloWorld,
+  const portTabActive = chrome.tabs.connect(tab.id, { name: "popup" });
+
+  portTabActive.onMessage.addListener(({message}) => {
+    pMessageElement.innerText = JSON.stringify(message,null,2);
   });
+
+    portTabActive.postMessage({ cmd: "scrapt" });
+
 });
 
-function alertHelloWorld() {
-  alert("Hello World");
-}
+

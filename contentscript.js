f862af9f71516b685dd0b3ad033f0a5a57833e5b
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2023-07-25 22:03:30 -0500

    [feat] add scrap jobs to comunication

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,55 @@
 console.log("Ejecutando el content script 1.0");
+function scrapJobs() {
+  let jobElements = document.querySelectorAll('div[id|="jobcard"]');
+  jobElements = [...jobElements];
+  const jobs = jobElements.map((card) => {
+    const [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              ,
+              { innerText: jobTitle },
+              { innerText: salary },
+              ,
+              elementCompanyLocation1,
+              elementCompanyLocation2,
+            ],
+          },
+        ],
+      },
+    ] = card.children;
 
-chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function ({ cmd }) {
-    alert(`${port.name}: message`);
+    const elementCompanyLocation = !elementCompanyLocation1?.querySelector("p")
+      ? elementCompanyLocation2
+      : elementCompanyLocation1;
+
+    const { innerText: company = null } =
+      elementCompanyLocation?.querySelector("label");
+    const { innerText: location = null } =
+      elementCompanyLocation?.querySelector("p");
+
+    return { url, jobTitle, salary, company, location };
+  });
+
+  let information = document.querySelector("#search-success > p");
+  information.innerHTML = jobs
+    .map(
+      (el) => `<a href='${el.url}'><p>${el.jobTitle}</p>
+    <p>${el.salary}</p>
+    <p>${el.company}</p>
+    <p>${el.location}</p>
+    </a>`
+    )
+    .join("");
+}
+
+chrome.runtime.onConnect.addListener((port) => {
+  port.onMessage.addListener(({ cmd }) => {
     if (cmd === "scrap-jobs") {
-      port.postMessage({ message: "Como estas?" });
+      scrapJobs();
+      port.postMessage({ success: true, message: "termino el scrapt" });
     }
   });
 });
--- popup/index.js
+++ popup/index.js
@@ -4,7 +4,7 @@ btnScrap.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let port = chrome.tabs.connect(tab.id, { name: "popup" });
   port.postMessage({ cmd: "scrap-jobs" });
-  port.onMessage.addListener(function ({ message }) {
-    alert(message);
+  port.onMessage.addListener(function ({ success, message }) {
+    if (success) alert(message);
   });
 });
